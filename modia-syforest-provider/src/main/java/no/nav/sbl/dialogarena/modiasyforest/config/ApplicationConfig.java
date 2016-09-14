afd92be09a775e2c59e01d4b47a7a8ee0e0c3d14
Andreas Lund <andreas.lund@nav.no>
2016-09-14 11:42:54 +0200

    Må ha isAlive servlet her.

--- modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/config/ApplicationConfig.java
+++ modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package no.nav.sbl.dialogarena.modiasyforest.config;
 import no.nav.metrics.aspects.CountAspect;
 import no.nav.metrics.aspects.TimerAspect;
 import no.nav.sbl.dialogarena.modiasyforest.selftest.HealthCheckService;
+import no.nav.sbl.dialogarena.modiasyforest.selftest.IsAliveServlet;
 import no.nav.tjeneste.virksomhet.aktoer.v2.binding.Aktoer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,5 +33,10 @@ public class ApplicationConfig {
     @Bean
     public HealthCheckService healthCheckService() {
         return new HealthCheckService();
+    }
+
+    @Bean
+    public IsAliveServlet isAliveServlet() {
+        return new IsAliveServlet();
     }
 }
