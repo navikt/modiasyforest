4cff14b29bb41636f4181d73bd820a64ccce130a
Andreas Lund <andreas.lund@nav.no>
2016-10-25 15:35:25 +0200

    Legger til arbeidsgivere som ikke har svart i nærmesteleder-lista.

--- modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/NaermestelederRessurs.java
+++ modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/NaermestelederRessurs.java
@@ -2,7 +2,10 @@ package no.nav.sbl.dialogarena.modiasyforest.rest;
 
 import no.nav.metrics.aspects.Timed;
 import no.nav.sbl.dialogarena.modiasyforest.rest.domain.Naermesteleder;
+import no.nav.sbl.dialogarena.modiasyforest.rest.domain.sykmelding.Sykmelding;
 import no.nav.sbl.dialogarena.modiasyforest.services.NaermesteLederService;
+import no.nav.sbl.dialogarena.modiasyforest.services.SykmeldingService;
+import no.nav.tjeneste.virksomhet.sykmelding.v1.informasjon.WSSkjermes;
 import org.springframework.stereotype.Controller;
 
 import javax.inject.Inject;
@@ -10,8 +13,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import java.util.Arrays;
 import java.util.List;
 
+import static java.util.Arrays.*;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Controller
@@ -21,11 +26,15 @@ public class NaermestelederRessurs {
 
     @Inject
     private NaermesteLederService naermesteLederService;
+    @Inject
+    private SykmeldingService sykmeldingService;
 
     @GET
     @Timed
     public List<Naermesteleder> hentNaermesteledere(@QueryParam("fnr") String fnr) {
+        List<Sykmelding> sykmeldinger = sykmeldingService.hentSykmeldinger(fnr, asList(WSSkjermes.SKJERMES_FOR_ARBEIDSGIVER));
         List<Naermesteleder> naermesteledere = naermesteLederService.hentNaermesteledere(fnr);
+        naermesteledere.addAll(naermesteLederService.hentOrganisasjonerSomIkkeHarSvart(naermesteledere, sykmeldinger));
         int idcounter = 0;
         for (Naermesteleder naermesteleder : naermesteledere) {
             naermesteleder.id = idcounter++;
--- modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/services/NaermesteLederService.java
+++ modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/services/NaermesteLederService.java
@@ -1,11 +1,14 @@
 package no.nav.sbl.dialogarena.modiasyforest.services;
 
+import no.nav.sbl.dialogarena.modiasyforest.rest.domain.Arbeidsgiver;
 import no.nav.sbl.dialogarena.modiasyforest.rest.domain.Naermesteleder;
+import no.nav.sbl.dialogarena.modiasyforest.rest.domain.sykmelding.Sykmelding;
 import no.nav.tjeneste.virksomhet.sykefravaersoppfoelging.v1.SykefravaersoppfoelgingV1;
 import no.nav.tjeneste.virksomhet.sykefravaersoppfoelging.v1.meldinger.WSHentNaermesteLederListeRequest;
 
 import javax.inject.Inject;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 import static no.nav.sbl.dialogarena.modiasyforest.mappers.NaermesteLederMapper.tilNaermesteLeder;
@@ -29,5 +32,25 @@ public class NaermesteLederService {
                         organisasjonService.hentNavn(element.getOrgnummer()),
                         fnr.substring(0, 6)))
                 .collect(toList());
+    }
+
+    public List<Naermesteleder> hentOrganisasjonerSomIkkeHarSvart(List<Naermesteleder> naermesteledere, List<Sykmelding> sykmeldinger) {
+        return sykmeldinger.stream()
+                .filter(sykmelding -> "SENDT".equals(sykmelding.status))
+                .filter(sykmelding -> !naermesteledere.stream()
+                        .filter(naermesteleder -> sykmelding.orgnummer.equals(naermesteleder.arbeidsgiver.orgnummer))
+                        .findAny()
+                        .isPresent())
+                .map(sykmelding -> new Naermesteleder()
+                        .withArbeidsgiver(new Arbeidsgiver()
+                                .withOrgnummer(sykmelding.orgnummer)
+                                .withNavn(sykmelding.innsendtArbeidsgivernavn))
+                        .withNavn("Ikke meldt inn")
+                        .withEpost("Ikke meldt inn")
+                        .withFodselsdato("Ikke meldt inn")
+                        .withTlf("Ikke meldt inn"))
+                .collect(toList());
+
+
     }
 }
