527397052c985138361bb0a0cc93e11d78b841db
Andreas Lund <andreas.lund@nav.no>
2016-10-25 14:02:51 +0200

    Json tilbake til Frontend.

--- modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/TilgangRessurs.java
+++ modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/TilgangRessurs.java
@@ -1,24 +1,28 @@
 package no.nav.sbl.dialogarena.modiasyforest.rest;
 
+import no.nav.sbl.dialogarena.modiasyforest.rest.domain.Tilgang;
 import org.springframework.stereotype.Controller;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import static java.lang.System.getProperty;
 import static java.util.Arrays.asList;
 import static no.nav.modig.core.context.SubjectHandler.getSubjectHandler;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Controller
 @Path("/toggle/tilgangmoteadmin")
+@Produces(APPLICATION_JSON)
 public class TilgangRessurs {
 
     @GET
-    public Boolean harSaksbehandlerTilgang() {
+    public Tilgang harSaksbehandlerTilgang() {
         String veildere = getProperty("tilgang.mote");
-        return veildere != null && asList(veildere.split(",")).stream()
+        return new Tilgang().withTilgang(veildere != null && asList(veildere.split(",")).stream()
                 .filter(s -> s.equals(getSubjectHandler().getUid()))
                 .findAny()
-                .isPresent();
+                .isPresent());
     }
 }
---
+++ modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/domain/Tilgang.java
@@ -0,0 +1,11 @@
+package no.nav.sbl.dialogarena.modiasyforest.rest.domain;
+
+public class Tilgang {
+
+    public Boolean harTilgang;
+
+    public Tilgang withTilgang(Boolean harTilgang) {
+        this.harTilgang = harTilgang;
+        return this;
+    }
+}
