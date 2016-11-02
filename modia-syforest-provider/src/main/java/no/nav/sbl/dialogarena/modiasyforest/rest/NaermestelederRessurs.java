d0253b9ef4dbc6be292a02669937756fc0c83cd7
Andreas Lund <andreas.lund@nav.no>
2016-11-02 14:11:44 +0100

    Skal ikke hente arbeidsgivere som ikke har svart til denne releasen da policyene for SykmeldingV1 er på plass for dette enda.

--- modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/NaermestelederRessurs.java
+++ modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/NaermestelederRessurs.java
@@ -2,10 +2,7 @@ package no.nav.sbl.dialogarena.modiasyforest.rest;
 
 import no.nav.metrics.aspects.Timed;
 import no.nav.sbl.dialogarena.modiasyforest.rest.domain.Naermesteleder;
-import no.nav.sbl.dialogarena.modiasyforest.rest.domain.sykmelding.Sykmelding;
 import no.nav.sbl.dialogarena.modiasyforest.services.NaermesteLederService;
-import no.nav.sbl.dialogarena.modiasyforest.services.SykmeldingService;
-import no.nav.tjeneste.virksomhet.sykmelding.v1.informasjon.WSSkjermes;
 import org.springframework.stereotype.Controller;
 
 import javax.inject.Inject;
@@ -13,12 +10,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
-import static java.lang.System.*;
-import static java.util.Arrays.*;
+import static java.lang.System.getProperty;
 import static java.util.Collections.emptyList;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
@@ -29,8 +23,8 @@ public class NaermestelederRessurs {
 
     @Inject
     private NaermesteLederService naermesteLederService;
-    @Inject
-    private SykmeldingService sykmeldingService;
+//    @Inject
+//    private SykmeldingService sykmeldingService;
 
     @GET
     @Timed
@@ -38,9 +32,9 @@ public class NaermestelederRessurs {
         if ("true".equals(getProperty("toggle.naermesteleder.veileder"))) {
             return emptyList();
         }
-        List<Sykmelding> sykmeldinger = sykmeldingService.hentSykmeldinger(fnr, asList(WSSkjermes.SKJERMES_FOR_ARBEIDSGIVER));
+//        List<Sykmelding> sykmeldinger = sykmeldingService.hentSykmeldinger(fnr, asList(WSSkjermes.SKJERMES_FOR_ARBEIDSGIVER));
         List<Naermesteleder> naermesteledere = naermesteLederService.hentNaermesteledere(fnr);
-        naermesteledere.addAll(naermesteLederService.hentOrganisasjonerSomIkkeHarSvart(naermesteledere, sykmeldinger));
+//        naermesteledere.addAll(naermesteLederService.hentOrganisasjonerSomIkkeHarSvart(naermesteledere, sykmeldinger));
         int idcounter = 0;
         for (Naermesteleder naermesteleder : naermesteledere) {
             naermesteleder.id = idcounter++;
