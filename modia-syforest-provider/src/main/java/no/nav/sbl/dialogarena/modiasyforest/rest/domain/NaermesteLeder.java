6bf33bd9129d2cdcb57ff493bdc36117406c467b
Andreas Lund <andreas.lund@nav.no>
2016-11-11 13:30:16 +0100

    Oppretter klassen igjen med riktig navn.

---
+++ modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/domain/NaermesteLeder.java
@@ -0,0 +1,63 @@
+package no.nav.sbl.dialogarena.modiasyforest.rest.domain;
+
+
+import java.time.LocalDate;
+
+public class NaermesteLeder {
+
+    public String navn;
+    public Integer id;
+    public String tlf;
+    public String epost;
+    public Boolean aktiv;
+    public Boolean erOppgitt;
+    public LocalDate fomDato;
+    public String orgnummer;
+    public String organisasjonsNavn;
+
+    public NaermesteLeder withNavn(String navn) {
+        this.navn = navn;
+        return this;
+    }
+
+    public NaermesteLeder withTlf(String tlf) {
+        this.tlf = tlf;
+        return this;
+    }
+
+    public NaermesteLeder withEpost(String epost) {
+        this.epost = epost;
+        return this;
+    }
+
+    public NaermesteLeder withErOppgitt(Boolean erOppgitt) {
+        this.erOppgitt = erOppgitt;
+        return this;
+    }
+
+    public NaermesteLeder withAktiv(Boolean aktiv) {
+        this.aktiv = aktiv;
+        return this;
+    }
+
+    public NaermesteLeder withOrgnummer(final String orgnummer) {
+        this.orgnummer = orgnummer;
+        return this;
+    }
+
+    public NaermesteLeder withOrganisasjonsnavn(final String organisasjonsNavn) {
+        this.organisasjonsNavn = organisasjonsNavn;
+        return this;
+    }
+
+    public NaermesteLeder withId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public NaermesteLeder withFomDato(LocalDate fomDato) {
+        this.fomDato = fomDato;
+        return this;
+    }
+
+}
