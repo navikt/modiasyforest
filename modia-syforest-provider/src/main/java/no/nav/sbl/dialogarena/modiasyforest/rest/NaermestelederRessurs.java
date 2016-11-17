091340b8ad8eea349357a9b133c2d03f5aca4e92
Andreas Lund <andreas.lund@nav.no>
2016-11-17 14:26:07 +0100

    Kommenterer ut disse for neste release.

--- modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/NaermestelederRessurs.java
+++ modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/NaermestelederRessurs.java
@@ -2,10 +2,7 @@ package no.nav.sbl.dialogarena.modiasyforest.rest;
 
 import no.nav.metrics.aspects.Timed;
 import no.nav.sbl.dialogarena.modiasyforest.rest.domain.NaermesteLeder;
-import no.nav.sbl.dialogarena.modiasyforest.rest.domain.sykmelding.Sykmelding;
 import no.nav.sbl.dialogarena.modiasyforest.services.NaermesteLederService;
-import no.nav.sbl.dialogarena.modiasyforest.services.SykmeldingService;
-import no.nav.tjeneste.virksomhet.sykmelding.v1.informasjon.WSSkjermes;
 import org.springframework.stereotype.Controller;
 
 import javax.inject.Inject;
@@ -15,7 +12,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import java.util.List;
 
-import static java.util.Arrays.asList;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Controller
@@ -25,15 +21,15 @@ public class NaermestelederRessurs {
 
     @Inject
     private NaermesteLederService naermesteLederService;
-    @Inject
-    private SykmeldingService sykmeldingService;
+//    @Inject
+//    private SykmeldingService sykmeldingService;
 
     @GET
     @Timed
     public List<NaermesteLeder> hentNaermesteledere(@QueryParam("fnr") String fnr) {
-        List<Sykmelding> sykmeldinger = sykmeldingService.hentSykmeldinger(fnr, asList(WSSkjermes.SKJERMES_FOR_ARBEIDSGIVER));
+//        List<Sykmelding> sykmeldinger = sykmeldingService.hentSykmeldinger(fnr, asList(WSSkjermes.SKJERMES_FOR_ARBEIDSGIVER));
         List<NaermesteLeder> naermesteledere = naermesteLederService.hentNaermesteledere(fnr);
-        naermesteledere.addAll(naermesteLederService.hentOrganisasjonerSomIkkeHarSvart(naermesteledere, sykmeldinger));
+//        naermesteledere.addAll(naermesteLederService.hentOrganisasjonerSomIkkeHarSvart(naermesteledere, sykmeldinger));
         long idcounter = 0;
         for (NaermesteLeder naermesteleder : naermesteledere) {
             naermesteleder.id = idcounter++;
