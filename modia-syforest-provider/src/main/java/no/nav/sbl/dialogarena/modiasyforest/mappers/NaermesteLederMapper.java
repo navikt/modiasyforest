98ce36e2ba40ea04bbfb9fa4b9d5bbff0702e83f
Andreas Lund <andreas.lund@nav.no>
2016-11-21 14:46:41 +0100

    capitalizer nærmeste leder navn.

--- modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/mappers/NaermesteLederMapper.java
+++ modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/mappers/NaermesteLederMapper.java
@@ -2,12 +2,12 @@ package no.nav.sbl.dialogarena.modiasyforest.mappers;
 
 import no.nav.sbl.dialogarena.modiasyforest.rest.domain.NaermesteLeder;
 import no.nav.tjeneste.virksomhet.sykefravaersoppfoelging.v1.informasjon.WSNaermesteLederListeElement;
-
+import static org.apache.commons.lang3.text.WordUtils.capitalize;
 public class NaermesteLederMapper {
 
     public static NaermesteLeder tilNaermesteLeder(WSNaermesteLederListeElement response, String organisasjon) {
         return new NaermesteLeder()
-                .withNavn(response.getNavn())
+                .withNavn(capitalize(response.getNavn()))
                 .withEpost(response.getEpost())
                 .withTlf(response.getMobil())
                 .withFomDato(response.getAktivFom())
