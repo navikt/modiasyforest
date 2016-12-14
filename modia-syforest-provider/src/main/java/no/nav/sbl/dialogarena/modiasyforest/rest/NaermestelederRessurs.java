b1b9d74bcc7bbaf14b5635d62aadcd8b39ddc804
Andreas Lund <andreas.lund@nav.no>
2016-12-14 14:57:58 +0100

    Håndterer om brukeren ikke har tilgang til sykmeldingene.

--- modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/NaermestelederRessurs.java
+++ modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/NaermestelederRessurs.java
@@ -3,10 +3,13 @@ package no.nav.sbl.dialogarena.modiasyforest.rest;
 import no.nav.metrics.aspects.Timed;
 import no.nav.sbl.dialogarena.modiasyforest.rest.domain.NaermesteLeder;
 import no.nav.sbl.dialogarena.modiasyforest.rest.domain.sykmelding.Sykmelding;
+import no.nav.sbl.dialogarena.modiasyforest.rest.feil.SyfoException;
+import no.nav.sbl.dialogarena.modiasyforest.services.AktoerService;
 import no.nav.sbl.dialogarena.modiasyforest.services.NaermesteLederService;
 import no.nav.sbl.dialogarena.modiasyforest.services.SykmeldingService;
 import no.nav.tjeneste.virksomhet.sykefravaersoppfoelging.v1.HentNaermesteLederListeSikkerhetsbegrensning;
 import no.nav.tjeneste.virksomhet.sykmelding.v1.informasjon.WSSkjermes;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 
 import javax.inject.Inject;
@@ -14,15 +17,19 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Controller
 @Path("/naermesteleder")
 @Produces(APPLICATION_JSON)
 public class NaermestelederRessurs {
+    private static final Logger LOG = getLogger(NaermestelederRessurs.class);
 
     @Inject
     private NaermesteLederService naermesteLederService;
@@ -32,7 +39,12 @@ public class NaermestelederRessurs {
     @GET
     @Timed
     public List<NaermesteLeder> hentNaermesteledere(@QueryParam("fnr") String fnr) throws HentNaermesteLederListeSikkerhetsbegrensning {
-        List<Sykmelding> sykmeldinger = sykmeldingService.hentSykmeldinger(fnr, asList(WSSkjermes.SKJERMES_FOR_ARBEIDSGIVER));
+        List<Sykmelding> sykmeldinger = new ArrayList<>();
+        try {
+            sykmeldinger = sykmeldingService.hentSykmeldinger(fnr, asList(WSSkjermes.SKJERMES_FOR_ARBEIDSGIVER));
+        } catch (SyfoException e) {
+            LOG.warn("Brukeren har ikke tilgang til sykmeldingene, men vi viser nærmeste leder allikevel.");
+        }
         List<NaermesteLeder> naermesteledere = naermesteLederService.hentNaermesteledere(fnr);
         naermesteledere.addAll(naermesteLederService.hentOrganisasjonerSomIkkeHarSvart(naermesteledere, sykmeldinger));
         long idcounter = 0;
