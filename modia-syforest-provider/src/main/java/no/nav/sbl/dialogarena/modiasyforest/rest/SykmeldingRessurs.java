25d2fcb8f121194a2395067eff850f54d14ab361
Andreas Lund <andreas.lund@nav.no>
2017-01-30 14:08:46 +0100

    tar imot type som et optional parameter

--- modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/SykmeldingRessurs.java
+++ modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/SykmeldingRessurs.java
@@ -1,19 +1,20 @@
 package no.nav.sbl.dialogarena.modiasyforest.rest;
 
 
-
 import no.nav.sbl.dialogarena.modiasyforest.rest.domain.sykmelding.Sykmelding;
 import no.nav.sbl.dialogarena.modiasyforest.services.SykmeldingService;
+import no.nav.tjeneste.virksomhet.sykmelding.v1.informasjon.WSSkjermes;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.Collections.emptyList;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static no.nav.tjeneste.virksomhet.sykmelding.v1.informasjon.WSSkjermes.SKJERMES_FOR_ARBEIDSGIVER;
 
 @Path("/sykmeldinger")
 @Produces(APPLICATION_JSON)
@@ -24,7 +25,11 @@ public class SykmeldingRessurs {
     private SykmeldingService sykmeldingService;
 
     @GET
-    public List<Sykmelding> hentSykmeldinger(@QueryParam("fnr") String fnr) {
-        return sykmeldingService.hentSykmeldinger(fnr, emptyList());
+    public List<Sykmelding> hentSykmeldinger(@QueryParam("fnr") String fnr, @QueryParam("type") String type) {
+        List<WSSkjermes> filter = new ArrayList<>();
+        if ("arbeidsgiver".equals(type)) {
+            filter.add(SKJERMES_FOR_ARBEIDSGIVER);
+        }
+        return sykmeldingService.hentSykmeldinger(fnr, filter);
     }
 }
