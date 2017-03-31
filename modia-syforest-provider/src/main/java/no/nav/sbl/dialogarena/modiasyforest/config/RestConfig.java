51c90eb8fa80ac32c4c7137239b6a975cd7e6f00
Andreas Lund <andreas.lund@nav.no>
2017-03-31 07:43:01 +0200

    bytter til rest-tjensten i oidclogin appen.

--- modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/config/RestConfig.java
+++ modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/config/RestConfig.java
@@ -1,7 +1,6 @@
 package no.nav.sbl.dialogarena.modiasyforest.config;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
-import no.nav.brukerdialog.isso.RelyingPartyCallback;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class RestConfig extends ResourceConfig {
@@ -9,6 +8,5 @@ public class RestConfig extends ResourceConfig {
     public RestConfig() {
         packages("no.nav.sbl.dialogarena.modiasyforest.rest");
         register(JacksonJaxbJsonProvider.class);
-        register(RelyingPartyCallback.class);
     }
 }
