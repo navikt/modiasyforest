4d0f0a901464c930353092fea06d9eaf4d348221
Andreas Lund <andreas.lund@nav.no>
2017-12-02 07:27:54 +0100

    Mocker brukerprofil

--- modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/mocks/BrukerprofilMock.java
+++ modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/mocks/BrukerprofilMock.java
@@ -4,6 +4,8 @@ import no.nav.tjeneste.virksomhet.brukerprofil.v3.BrukerprofilV3;
 import no.nav.tjeneste.virksomhet.brukerprofil.v3.HentKontaktinformasjonOgPreferanserPersonIdentErUtgaatt;
 import no.nav.tjeneste.virksomhet.brukerprofil.v3.HentKontaktinformasjonOgPreferanserPersonIkkeFunnet;
 import no.nav.tjeneste.virksomhet.brukerprofil.v3.HentKontaktinformasjonOgPreferanserSikkerhetsbegrensning;
+import no.nav.tjeneste.virksomhet.brukerprofil.v3.informasjon.WSBruker;
+import no.nav.tjeneste.virksomhet.brukerprofil.v3.informasjon.WSPersonnavn;
 import no.nav.tjeneste.virksomhet.brukerprofil.v3.meldinger.WSHentKontaktinformasjonOgPreferanserRequest;
 import no.nav.tjeneste.virksomhet.brukerprofil.v3.meldinger.WSHentKontaktinformasjonOgPreferanserResponse;
 
@@ -15,6 +17,10 @@ public class BrukerprofilMock implements BrukerprofilV3 {
 
     @Override
     public WSHentKontaktinformasjonOgPreferanserResponse hentKontaktinformasjonOgPreferanser(WSHentKontaktinformasjonOgPreferanserRequest request) throws HentKontaktinformasjonOgPreferanserSikkerhetsbegrensning, HentKontaktinformasjonOgPreferanserPersonIkkeFunnet, HentKontaktinformasjonOgPreferanserPersonIdentErUtgaatt {
-        return null;
+        return new WSHentKontaktinformasjonOgPreferanserResponse()
+                .withBruker(new WSBruker()
+                        .withPersonnavn(new WSPersonnavn()
+                                .withFornavn("Test")
+                                .withEtternavn("Testesen")));
     }
 }
