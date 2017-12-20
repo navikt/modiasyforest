4d9c8f974cc8f125385a9b12d7fbda1ed98059ed
Andreas Lund <andreas.lund@nav.no>
2017-12-20 12:40:10 +0100

    Henter og setter JWT på sikkerhetskonteksten ved lokal kjøring.

--- modia-syforest-provider/src/test/java/StartJetty.java
+++ modia-syforest-provider/src/test/java/StartJetty.java
@@ -1,14 +1,15 @@
 import no.nav.brukerdialog.security.context.InternbrukerSubjectHandler;
+import no.nav.brukerdialog.security.domain.OidcCredential;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
-import org.apache.geronimo.components.jaspi.AuthConfigFactoryImpl;
 
-import javax.security.auth.message.config.AuthConfigFactory;
 import java.io.File;
-import java.security.Security;
+import java.io.IOException;
 
+import static java.lang.System.getProperty;
 import static java.lang.System.setProperty;
-import static no.nav.brukerdialog.security.context.InternbrukerSubjectHandler.setServicebruker;
+import static no.nav.brukerdialog.security.context.InternbrukerSubjectHandler.setOidcCredential;
 import static no.nav.brukerdialog.security.context.InternbrukerSubjectHandler.setVeilederIdent;
+import static no.nav.brukerdialog.tools.ISSOProvider.getIDToken;
 import static no.nav.modig.core.test.FilesAndDirs.TEST_RESOURCES;
 import static no.nav.modig.core.test.FilesAndDirs.WEBAPP_SOURCE;
 import static no.nav.modig.lang.collections.FactoryUtils.gotKeypress;
@@ -21,26 +22,16 @@ import static no.nav.sbl.dialogarena.test.SystemProperties.setFrom;
 
 public class StartJetty {
 
-    // Ta Edit Confiugration på StartJetty. Sett Provider-modulen som working directory
-
-    public static void main(String[] args) {
-        runJetty();
-    }
-
-    private static void runJetty() {
+    public static void main(String[] args) throws IOException {
         setFrom("jetty-environment.properties");
         setupKeyAndTrustStore();
-
-        setVeilederIdent("***REMOVED***");
-        setServicebruker("srvmodiasyforest");
         setProperty("no.nav.brukerdialog.security.context.subjectHandlerImplementationClass", InternbrukerSubjectHandler.class.getName());
-        setProperty("org.apache.geronimo.jaspic.configurationFile", "src/test/resources/jaspiconf.xml");
-        Security.setProperty(AuthConfigFactory.DEFAULT_FACTORY_SECURITY_PROPERTY, AuthConfigFactoryImpl.class.getCanonicalName());
+        setVeilederIdent(getProperty("veileder.username"));
+        setOidcCredential(new OidcCredential(getIDToken()));
 
         Jetty jetty = usingWar(WEBAPP_SOURCE)
                 .at("modiasyforest")
                 .port(8084)
-                .configureForJaspic()
                 .overrideWebXml(new File(TEST_RESOURCES, "override-web.xml"))
                 .buildJetty();
         jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
