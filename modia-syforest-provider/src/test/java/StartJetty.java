114b11062d72320b5aead81e218e56e74b17dedc
Andreas Lund <andreas.lund@nav.no>
2017-12-21 10:39:56 +0100

    Bumper common-cxf til digisyfo-edition

--- modia-syforest-provider/src/test/java/StartJetty.java
+++ modia-syforest-provider/src/test/java/StartJetty.java
@@ -1,5 +1,4 @@
-import no.nav.brukerdialog.security.context.InternbrukerSubjectHandler;
-import no.nav.brukerdialog.security.domain.OidcCredential;
+import no.nav.brukerdialog.security.context.CustomizableSubjectHandler;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
 
 import java.io.File;
@@ -7,15 +6,14 @@ import java.io.IOException;
 
 import static java.lang.System.getProperty;
 import static java.lang.System.setProperty;
-import static no.nav.brukerdialog.security.context.InternbrukerSubjectHandler.setOidcCredential;
-import static no.nav.brukerdialog.security.context.InternbrukerSubjectHandler.setVeilederIdent;
+import static no.nav.brukerdialog.security.context.CustomizableSubjectHandler.setInternSsoToken;
+import static no.nav.brukerdialog.security.context.CustomizableSubjectHandler.setUid;
 import static no.nav.brukerdialog.tools.ISSOProvider.getIDToken;
 import static no.nav.modig.core.test.FilesAndDirs.TEST_RESOURCES;
 import static no.nav.modig.core.test.FilesAndDirs.WEBAPP_SOURCE;
 import static no.nav.modig.lang.collections.FactoryUtils.gotKeypress;
 import static no.nav.modig.lang.collections.RunnableUtils.first;
 import static no.nav.modig.lang.collections.RunnableUtils.waitFor;
-import static no.nav.modig.testcertificates.TestCertificates.setupKeyAndTrustStore;
 import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
 import static no.nav.sbl.dialogarena.test.SystemProperties.setFrom;
 
@@ -24,10 +22,9 @@ public class StartJetty {
 
     public static void main(String[] args) throws IOException {
         setFrom("jetty-environment.properties");
-        setupKeyAndTrustStore();
-        setProperty("no.nav.brukerdialog.security.context.subjectHandlerImplementationClass", InternbrukerSubjectHandler.class.getName());
-        setVeilederIdent(getProperty("veileder.username"));
-        setOidcCredential(new OidcCredential(getIDToken()));
+        setProperty("no.nav.brukerdialog.security.context.subjectHandlerImplementationClass", CustomizableSubjectHandler.class.getName());
+        setUid(getProperty("veileder.username"));
+        setInternSsoToken(getIDToken());
 
         Jetty jetty = usingWar(WEBAPP_SOURCE)
                 .at("modiasyforest")
