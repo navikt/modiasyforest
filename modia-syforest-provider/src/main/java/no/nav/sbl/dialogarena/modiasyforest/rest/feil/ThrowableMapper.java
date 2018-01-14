202d287c93fdca5d926ff995208e65ca17480bad
Andreas Lund <andreas.lund@nav.no>
2018-01-14 21:51:43 +0100

    Lager et metrics event dersom feilmeldingen som kastes er en 500-feil slik at dette enkelt kan trackes i Grafana.

--- modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/feil/ThrowableMapper.java
+++ modia-syforest-provider/src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/feil/ThrowableMapper.java
@@ -1,27 +1,25 @@
 package no.nav.sbl.dialogarena.modiasyforest.rest.feil;
 
-import org.slf4j.Logger;
-
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static no.nav.metrics.MetricsFactory.createEvent;
 import static no.nav.sbl.dialogarena.modiasyforest.rest.feil.Feil.GENERELL_FEIL;
 import static no.nav.sbl.dialogarena.modiasyforest.rest.feil.Feilmelding.NO_BIGIP_5XX_REDIRECT;
-import static org.slf4j.LoggerFactory.getLogger;
 
 @Provider
 public class ThrowableMapper implements ExceptionMapper<Throwable> {
 
-    private static final Logger LOG = getLogger(ThrowableMapper.class);
-
     @Override
     public Response toResponse(Throwable e) {
-        LOG.error("Uventet feil", e);
-
         Feilmelding melding = new Feilmelding().withFeil(GENERELL_FEIL);
+
+        if (statuskode(e) == 500) {
+            createEvent("runtimeexception").report();
+        }
 
         return Response.status(statuskode(e))
                 .type(APPLICATION_JSON)
