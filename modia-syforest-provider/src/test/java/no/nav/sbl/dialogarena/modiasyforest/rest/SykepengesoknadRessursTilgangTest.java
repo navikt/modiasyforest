81120c4b4c0bdf675f747effce9fe55cd4ea598d
Engelien, Stefan Andreas <stefan.andreas.engelien@nav.no>
2018-02-22 15:32:03 +0100

    SYFOUTV-1906 Bruke riktig feilmelding til front

--- modia-syforest-provider/src/test/java/no/nav/sbl/dialogarena/modiasyforest/rest/SykepengesoknadRessursTilgangTest.java
+++ modia-syforest-provider/src/test/java/no/nav/sbl/dialogarena/modiasyforest/rest/SykepengesoknadRessursTilgangTest.java
@@ -1,5 +1,8 @@
 package no.nav.sbl.dialogarena.modiasyforest.rest;
 
+import no.nav.sbl.dialogarena.modiasyforest.rest.domain.tilgang.Tilgang;
+import no.nav.sbl.dialogarena.modiasyforest.rest.feil.SyfoException;
+import no.nav.sbl.dialogarena.modiasyforest.rest.feil.SyfoTilgangException;
 import no.nav.sbl.dialogarena.modiasyforest.services.AktoerService;
 import no.nav.sbl.dialogarena.modiasyforest.services.SykepengesoknaderService;
 import org.glassfish.jersey.message.internal.Statuses;
@@ -7,10 +10,8 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import javax.ws.rs.ForbiddenException;
-import javax.ws.rs.WebApplicationException;
-
 import static java.util.Collections.emptyList;
+import static no.nav.sbl.dialogarena.modiasyforest.rest.domain.tilgang.AdRoller.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -37,14 +38,39 @@ public class SykepengesoknadRessursTilgangTest extends AbstractRessursTilgangTes
         verify(tilgangskontrollResponse).getStatus();
     }
 
-    @Test(expected = ForbiddenException.class)
-    public void har_ikke_tilgang() {
+    @Test(expected = SyfoTilgangException.class)
+    public void har_ikke_tilgang_kode_6() {
         when(tilgangskontrollResponse.getStatus()).thenReturn(403);
+        when(tilgangskontrollResponse.readEntity(Tilgang.class)).thenReturn(new Tilgang().ikkeTilgangGrunn(KODE6.name()));
 
         sykepengesoknadRessurs.hentSykepengesoknader(FNR);
     }
 
-    @Test(expected = WebApplicationException.class)
+    @Test(expected = SyfoTilgangException.class)
+    public void har_ikke_tilgang_kode_7() {
+        when(tilgangskontrollResponse.getStatus()).thenReturn(403);
+        when(tilgangskontrollResponse.readEntity(Tilgang.class)).thenReturn(new Tilgang().ikkeTilgangGrunn(KODE7.name()));
+
+        sykepengesoknadRessurs.hentSykepengesoknader(FNR);
+    }
+
+    @Test(expected = SyfoTilgangException.class)
+    public void har_ikke_tilgang_egenansatt() {
+        when(tilgangskontrollResponse.getStatus()).thenReturn(403);
+        when(tilgangskontrollResponse.readEntity(Tilgang.class)).thenReturn(new Tilgang().ikkeTilgangGrunn(EGEN_ANSATT.name()));
+
+        sykepengesoknadRessurs.hentSykepengesoknader(FNR);
+    }
+
+    @Test(expected = SyfoTilgangException.class)
+    public void har_ikke_tilgang_sensitiv() {
+        when(tilgangskontrollResponse.getStatus()).thenReturn(403);
+        when(tilgangskontrollResponse.readEntity(Tilgang.class)).thenReturn(new Tilgang().ikkeTilgangGrunn(SYFO.name()));
+
+        sykepengesoknadRessurs.hentSykepengesoknader(FNR);
+    }
+
+    @Test(expected = SyfoException.class)
     public void annen_tilgangsfeil() {
         when(tilgangskontrollResponse.getStatus()).thenReturn(500);
         when(tilgangskontrollResponse.getStatusInfo()).thenReturn(Statuses.from(500, "Tau i propellen"));
