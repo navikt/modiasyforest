dab2244a5e3ca6e17c72aba3e1cb9e213cd10c09
Andreas Lund <andreas.lund@nav.no>
2018-03-26 20:21:44 +0200

    Fiks så det vises i personkortet at en bruker er kode 6-7 selv om man ikke har tilgang

--- src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/DiskresjonskodeRessurs.java
+++ src/main/java/no/nav/sbl/dialogarena/modiasyforest/rest/DiskresjonskodeRessurs.java
@@ -1,7 +1,6 @@
 package no.nav.sbl.dialogarena.modiasyforest.rest;
 
 import no.nav.sbl.dialogarena.modiasyforest.services.DiskresjonskodeService;
-import no.nav.sbl.dialogarena.modiasyforest.services.TilgangService;
 import org.springframework.stereotype.Controller;
 
 import javax.inject.Inject;
@@ -16,14 +15,10 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 public class DiskresjonskodeRessurs {
 
     @Inject
-    private TilgangService tilgangService;
-
-    @Inject
     private DiskresjonskodeService diskresjonskodeService;
 
     @GET
     public DiskresjonskodeSvar hentDiskresjonskode(@PathParam("fnr") String fnr) {
-        tilgangService.sjekkTilgangTilPerson(fnr);
         return new DiskresjonskodeSvar(diskresjonskodeService.diskresjonskode(fnr));
     }
 
