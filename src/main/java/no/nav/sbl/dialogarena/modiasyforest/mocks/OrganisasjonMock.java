6227da1a8f6f1396b50dc2d68f84ecf625ed6204
John Martin Lindseth <john.martin.lindseth@nav.no>
2019-01-31 14:31:24 +0100

    Ny commit for at siste commit ikke skal ha feil data

--- src/main/java/no/nav/sbl/dialogarena/modiasyforest/mocks/OrganisasjonMock.java
+++ src/main/java/no/nav/sbl/dialogarena/modiasyforest/mocks/OrganisasjonMock.java
@@ -8,7 +8,7 @@ import no.nav.tjeneste.virksomhet.organisasjon.v4.meldinger.*;
 public class OrganisasjonMock implements OrganisasjonV4 {
     @Override
     public WSHentOrganisasjonResponse hentOrganisasjon(WSHentOrganisasjonRequest wsHentOrganisasjonRequest) throws HentOrganisasjonOrganisasjonIkkeFunnet, HentOrganisasjonUgyldigInput {
-        return new WSHentOrganisasjonResponse().withOrganisasjon(new WSOrganisasjon().withNavn(new WSUstrukturertNavn().withNavnelinje("TEST Consulting AS")));
+        return new WSHentOrganisasjonResponse().withOrganisasjon(new WSOrganisasjon().withNavn(new WSUstrukturertNavn().withNavnelinje("TEST Consult AS")));
     }
 
     @Override
