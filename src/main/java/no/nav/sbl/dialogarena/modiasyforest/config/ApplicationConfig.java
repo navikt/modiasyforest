68bd23c954116810f4bd51cc1e8d94300c921dc9
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-03-07 15:11:55 +0100

    Fjern @EnableAspectJAutoProxy

--- src/main/java/no/nav/sbl/dialogarena/modiasyforest/config/ApplicationConfig.java
+++ src/main/java/no/nav/sbl/dialogarena/modiasyforest/config/ApplicationConfig.java
@@ -2,14 +2,12 @@ package no.nav.sbl.dialogarena.modiasyforest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
 
 import static java.util.Arrays.asList;
 
 @Configuration
-@EnableAspectJAutoProxy
 public class ApplicationConfig {
 
     @Bean
