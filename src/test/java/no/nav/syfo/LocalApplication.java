a82ee171d1859a488d41bb9cab823df34512b3b7
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-10-03 15:58:23 +0200

    Remove redundant value

--- src/test/java/no/nav/syfo/LocalApplication.java
+++ src/test/java/no/nav/syfo/LocalApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableOIDCTokenValidation(ignore="org.springframework")
+@EnableOIDCTokenValidation
 public class LocalApplication {
     public static void main(String[] args) {
         SpringApplication.run(LocalApplication.class, args);
