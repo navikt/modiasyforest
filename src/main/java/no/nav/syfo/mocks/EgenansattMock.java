af512c86093fa2ba5c711aa59debc7d34c8944b4
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-11-13 13:19:37 +0100

    Add controller with azuread auth for EgenAnsatt

--- src/main/java/no/nav/syfo/mocks/EgenansattMock.java
+++ src/main/java/no/nav/syfo/mocks/EgenansattMock.java
@@ -1,15 +1,16 @@
 package no.nav.syfo.mocks;
 
 import no.nav.syfo.config.EgenAnsattConfig;
-import no.nav.tjeneste.pip.egen.ansatt.v1.EgenAnsattV1;
-import no.nav.tjeneste.pip.egen.ansatt.v1.WSHentErEgenAnsattEllerIFamilieMedEgenAnsattRequest;
-import no.nav.tjeneste.pip.egen.ansatt.v1.WSHentErEgenAnsattEllerIFamilieMedEgenAnsattResponse;
+import no.nav.tjeneste.pip.egen.ansatt.v1.*;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 @Service
 @ConditionalOnProperty(value = EgenAnsattConfig.MOCK_KEY, havingValue = "true")
 public class EgenansattMock implements EgenAnsattV1 {
+
+    public final static boolean IS_EGEN_ANSATT = true;
+
     @Override
     public void ping() {
 
@@ -17,6 +18,6 @@ public class EgenansattMock implements EgenAnsattV1 {
 
     @Override
     public WSHentErEgenAnsattEllerIFamilieMedEgenAnsattResponse hentErEgenAnsattEllerIFamilieMedEgenAnsatt(WSHentErEgenAnsattEllerIFamilieMedEgenAnsattRequest wsHentErEgenAnsattEllerIFamilieMedEgenAnsattRequest) {
-        return new WSHentErEgenAnsattEllerIFamilieMedEgenAnsattResponse().withEgenAnsatt(false);
+        return new WSHentErEgenAnsattEllerIFamilieMedEgenAnsattResponse().withEgenAnsatt(IS_EGEN_ANSATT);
     }
 }
