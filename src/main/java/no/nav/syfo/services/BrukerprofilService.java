d45973765288ba06a97b1080ee4dba6d0a2fccdb
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-12-06 11:45:09 +0100

    Remove unused function HentNavn in BrukerprofilService

--- src/main/java/no/nav/syfo/services/BrukerprofilService.java
+++ src/main/java/no/nav/syfo/services/BrukerprofilService.java
@@ -1,7 +1,8 @@
 package no.nav.syfo.services;
 
 import no.nav.tjeneste.virksomhet.brukerprofil.v3.*;
-import no.nav.tjeneste.virksomhet.brukerprofil.v3.informasjon.*;
+import no.nav.tjeneste.virksomhet.brukerprofil.v3.informasjon.WSBruker;
+import no.nav.tjeneste.virksomhet.brukerprofil.v3.informasjon.WSNorskIdent;
 import no.nav.tjeneste.virksomhet.brukerprofil.v3.meldinger.WSHentKontaktinformasjonOgPreferanserRequest;
 import org.slf4j.Logger;
 import org.springframework.cache.annotation.Cacheable;
@@ -11,9 +12,6 @@ import javax.inject.Inject;
 import javax.ws.rs.ForbiddenException;
 
 import static no.nav.syfo.config.CacheConfig.CACHENAME_TPSBRUKER;
-import static no.nav.syfo.config.CacheConfig.CACHENAME_TPSNAVN;
-import static org.apache.commons.lang3.StringUtils.isBlank;
-import static org.apache.commons.lang3.text.WordUtils.capitalize;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Service
@@ -26,37 +24,6 @@ public class BrukerprofilService {
     @Inject
     public BrukerprofilService(BrukerprofilV3 brukerprofilV3) {
         this.brukerprofilV3 = brukerprofilV3;
-    }
-
-    @Cacheable(cacheNames = CACHENAME_TPSNAVN, key = "#fnr", condition = "#fnr != null")
-    public String hentNavn(String fnr) {
-        if (isBlank(fnr) || !fnr.matches("\\d{11}$")) {
-            log.error("Prøvde å hente navn med fnr");
-            throw new IllegalArgumentException();
-        }
-        try {
-            WSPerson wsPerson = brukerprofilV3.hentKontaktinformasjonOgPreferanser(new WSHentKontaktinformasjonOgPreferanserRequest()
-                    .withIdent(new WSNorskIdent()
-                            .withIdent(fnr))).getBruker();
-            String mellomnavn = wsPerson.getPersonnavn().getMellomnavn() == null ? "" : wsPerson.getPersonnavn().getMellomnavn();
-            if (!"".equals(mellomnavn)) {
-                mellomnavn = mellomnavn + " ";
-            }
-            final String navnFraTps = wsPerson.getPersonnavn().getFornavn() + " " + mellomnavn + wsPerson.getPersonnavn().getEtternavn();
-            return capitalize(navnFraTps.toLowerCase(), '-', ' ');
-        } catch (HentKontaktinformasjonOgPreferanserPersonIdentErUtgaatt e) {
-            log.error("HentKontaktinformasjonOgPreferanserPersonIdentErUtgaatt med FNR", e);
-            throw new RuntimeException();
-        } catch (HentKontaktinformasjonOgPreferanserSikkerhetsbegrensning e) {
-            log.error("Sikkerhetsbegrensning med FNR", e);
-            throw new ForbiddenException();
-        } catch (HentKontaktinformasjonOgPreferanserPersonIkkeFunnet e) {
-            log.error("HentKontaktinformasjonOgPreferanserPersonIkkeFunnet med FNR", e);
-            throw new RuntimeException();
-        } catch (RuntimeException e) {
-            log.error("Fikk RuntimeException mot TPS med ved oppslag", e);
-            return "Vi fant ikke navnet";
-        }
     }
 
     @Cacheable(cacheNames = CACHENAME_TPSBRUKER, key = "#fnr", condition = "#fnr != null")
