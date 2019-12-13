ce17643e636a08dfefeccb035f574021b4698a83
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-12-13 12:48:38 +0100

    Call syfotilgangskontroll with bearer token

--- src/main/java/no/nav/syfo/services/TilgangService.java
+++ src/main/java/no/nav/syfo/services/TilgangService.java
@@ -1,6 +1,10 @@
 package no.nav.syfo.services;
 
+import no.nav.security.oidc.context.OIDCRequestContextHolder;
+import no.nav.syfo.oidc.OIDCIssuer;
+import no.nav.syfo.oidc.OIDCUtil;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -8,12 +12,16 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.ws.rs.ForbiddenException;
 import java.net.URI;
+import java.util.Collections;
 
 import static java.util.Collections.singletonMap;
+import static no.nav.syfo.util.CredentialUtilKt.bearerHeader;
 import static org.springframework.web.util.UriComponentsBuilder.fromHttpUrl;
 
 @Service
 public class TilgangService {
+
+    private final OIDCRequestContextHolder oidcContextHolder;
 
     public static final String FNR = "fnr";
     public static final String TILGANG_TIL_BRUKER_PATH = "/tilgangtilbruker";
@@ -26,9 +34,11 @@ public class TilgangService {
     private final UriComponentsBuilder accessToSYFOWithAzureUriTemplate;
 
     public TilgangService(
+            OIDCRequestContextHolder oidcContextHolder,
             @Value("${tilgangskontrollapi.url}") String tilgangskontrollUrl,
             RestTemplate template
     ) {
+        this.oidcContextHolder = oidcContextHolder;
         tilgangTilBrukerUriTemplate = fromHttpUrl(tilgangskontrollUrl)
                 .path(TILGANG_TIL_BRUKER_PATH)
                 .queryParam(FNR, FNR_PLACEHOLDER);
@@ -63,12 +73,30 @@ public class TilgangService {
 
     public boolean isVeilederGrantedAccessToUserWithAD(String fnr) {
         URI tilgangTilBrukerViaAzureUriMedFnr = tilgangTilBrukerViaAzureUriTemplate.build(singletonMap(FNR, fnr));
-        return kallUriMedTemplate(tilgangTilBrukerViaAzureUriMedFnr);
+        return checkAccess(tilgangTilBrukerViaAzureUriMedFnr, OIDCIssuer.AZURE);
     }
 
     public boolean isVeilederGrantedAccessToSYFO() {
         URI accessToSyfoUri = accessToSYFOWithAzureUriTemplate.build().toUri();
         return kallUriMedTemplate(accessToSyfoUri);
+    }
+
+    private boolean checkAccess(URI uri, String oidcIssuer) {
+        try {
+            template.exchange(
+                    uri,
+                    HttpMethod.GET,
+                    createEntity(oidcIssuer),
+                    String.class
+            );
+            return true;
+        } catch (HttpClientErrorException e) {
+            if (e.getRawStatusCode() == 403) {
+                return false;
+            } else {
+                throw e;
+            }
+        }
     }
 
     private boolean kallUriMedTemplate(URI uri) {
@@ -82,5 +110,12 @@ public class TilgangService {
                 throw e;
             }
         }
+    }
+
+    private HttpEntity<String> createEntity(String issuer) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.set("Authorization", bearerHeader(OIDCUtil.tokenFraOIDC(oidcContextHolder, issuer)));
+        return new HttpEntity<>(headers);
     }
 }
