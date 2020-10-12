5e6032d6c9ba0fac27a89ee64850c44353b87235
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-10-12 13:27:49 +0200

    Correct APP_CONSUMER_ID to application name

--- src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
+++ src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
@@ -3,7 +3,7 @@ package no.nav.syfo.util
 import java.util.*
 
 const val NAV_CONSUMER_ID_HEADER = "Nav-Consumer-Id"
-const val APP_CONSUMER_ID = "syfomoteadmin"
+const val APP_CONSUMER_ID = "modiasyforest"
 const val NAV_CALL_ID_HEADER = "Nav-Call-Id"
 
 const val NAV_PERSONIDENTER_HEADER = "Nav-Personidenter"
