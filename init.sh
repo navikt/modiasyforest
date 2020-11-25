818b12fa595be92bdd3bb8e43d0e3945b68da8d5
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-11-25 15:57:45 +0100

    Use serviceuser-secret to get serviceuser for Modiasyforest

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
 export CLIENT_ID=$(cat /secrets/azuread/modiasyforest/client_id)
 export CLIENT_SECRET=$(cat /secrets/azuread/modiasyforest/client_secret)
+
+export SRV_USERNAME=$(cat /secrets/serviceuser/modiasyforest/username)
+export SRV_PASSWORD=$(cat /secrets/serviceuser/modiasyforest/password)
